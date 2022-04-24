522b7b956835d7f6cf6d04aada92d3975b201e02
necklace <xiangxn@163.com>
2022-04-24 18:46:21 +0800

    add merge ours

--- scripts/distribution.sh
+++ scripts/distribution.sh
@@ -1,3 +1,4 @@
 . ./scripts/env.sh
-$BINARY query distribution commission $VALIDATOR -o json --node $RPC_NODE
+# $BINARY query distribution commission $VALIDATOR -o json --node $RPC_NODE
 $BINARY query distribution validator-outstanding-rewards $VALIDATOR -o json --node $RPC_NODE
+# $BINARY query distribution rewards $VALIDATOR -o json --node $RPC_NODE
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,9 +1,9 @@
 export NODE_HOME=$HOME/.gaia
 export BINARY=$HOME/work/nutbox/cosmos/gaiad
-export RPC_NODE="tcp://154.53.55.234:80"
-export ACCOUNT=cosmos13hule0xx8gvzjrgstqjct0kwygfh7kd05ehsa6
-export VALIDATOR=cosmosvaloper13hule0xx8gvzjrgstqjct0kwygfh7kd03dr93f
+export RPC_NODE="tcp://154.53.55.234:27657"
+export ACCOUNT=cosmos1cl2utqez598xt6gqz7wfrfjsw5t34f2jpshtjj
+export VALIDATOR=cosmosvaloper1cl2utqez598xt6gqz7wfrfjsw5t34f2jyyr77p
 export SHOW_ADDRESS=
-export CHAIN_ID=cosmoshub-4
-export FROM_NAME=nutbox
-export MONIKER="noogra.nuts"
+export CHAIN_ID=theta-testnet-001
+export FROM_NAME=testmain
+export MONIKER="test-nutbox-node"
